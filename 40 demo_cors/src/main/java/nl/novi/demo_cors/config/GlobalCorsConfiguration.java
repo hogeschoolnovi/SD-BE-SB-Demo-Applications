38f8b72cc9821d8d0ddd951d61ff091dc6797156
Peter Anema <email@peteranema.nl>
2021-12-16 16:47:41 +0100

    Demo Paging and Sorting

--- 40 demo_cors/src/main/java/nl/novi/demo_cors/config/GlobalCorsConfiguration.java
+++ 40 demo_cors/src/main/java/nl/novi/demo_cors/config/GlobalCorsConfiguration.java
@@ -9,16 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class GlobalCorsConfiguration
 {
-    @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer()
+//    {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+//            }
+//        };
+//    }
 }
